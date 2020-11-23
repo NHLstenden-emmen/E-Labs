69e8c9325e86d36c4049a939435d3ae7036d34fa
Kevin Smulders <smulders.kevin.1999@gmail.com>
2020-11-23 17:34:46 +0100

    added the colors and font

--- inc/header.php
+++ inc/header.php
@@ -22,6 +22,8 @@
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
 		
 		<!-- style sheet -->
+		<link rel="stylesheet" href="css/main/main.css">
+		<link rel="stylesheet" href="css/main/navbar.css">
 		<?php
 		switch($pagePath)
 		{
