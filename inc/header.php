9e03ef8afa606e34e85164ace0c9ccc2934aef76
Kevin Smulders <smulders.kevin.1999@gmail.com>
2020-11-28 14:54:12 +0100

    added the favicons

--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="nl">
     <head>
+		<!-- start favicon -->
+		<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+		<link rel="manifest" href="images/favicon/site.webmanifest">
+		<link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg" color="#005aa7">
+		<meta name="apple-mobile-web-app-title" content="E-Labs">
+		<meta name="application-name" content="E-Labs">
+		<meta name="msapplication-TileColor" content="#005aa7">
+		<meta name="theme-color" content="#ffffff">
+
 		<!-- meta tags -->
 		<meta charset="UTF-8">
 		<meta name="description" content="Free Web tutorials">
@@ -20,7 +31,7 @@
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 		<!-- Font Awesome CDN -->
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
-		
+
 		<!-- style sheet -->
 		<link rel="stylesheet" href="css/main/main.css">
 		<link rel="stylesheet" href="css/main/navbar.css">
