713fe067875645c147dd1bcd64fa8a95970e588f
MikeVverschuur <72789016+MikeVverschuur@users.noreply.github.com>
2020-11-30 21:21:18 +0100

    Begin login

---
+++ pages/login/homelogin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>Login</h1>
+        <form action="login.php" method="post">
+            <label>naam:</label>
+            <input type="text" name="name" required>
+            <label>wachtwoord:</label>
+            <input type="password" name="password" required>
+            <button type="submit">login</button>
+        </form>
+    </body>
+</html>
