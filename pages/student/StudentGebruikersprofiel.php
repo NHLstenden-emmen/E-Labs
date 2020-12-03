daa4022f46afe1a56e1474f12a6fdb37e568290a
esmeekajuiter <esmeekajuiter@gmail.com>
2020-12-03 14:43:27 +0100

    Pagina opzetten

---
+++ pages/student/StudentGebruikersprofiel.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="../../css/pages/Gebruikersprofiel.css">
+		<link rel="stylesheet" type="text/css" href="">
+		<title> Gebruikersprofiel </title>
+		<!-- Hier komt de navbar -->
+	</head>
+	<body>
+
+
+		<h1>Gebruikersprofiel</h1>
+		<img src="../../images/unknown.png" alt="Profielfoto">
+		<h2>Profielfoto aanpassen</h2>
+		<div class="Gebruikersprofielcontainer">
+			<p id="Gebruikersprofielstudentinformatierechts"> Studentnummer:
+				</br> 564234
+				</br> Voornaam:
+				</br> Piet
+				</br> Achternaam:
+				</br> Klaassen
+				</br> Klas:
+				</br> INF1A
+			</p>
+			<p id="Gebruikersprofielstudentinformatielinks"> Studie:
+				</br> Informatica
+				</br> E-mail:
+				</br> Student@student.nhlstenden.nl
+				</br> Jaar:
+				</br> Jaar 1
+				</br> Wachtwoord:
+				</br> ********************
+			</p>
+		</div>
+		<h1>Opslaan</h1>
+
+
+		<!-- Hier komt de titel van de pagina -->
+
+
+		<!-- Hier komt de inhoud van de pagina -->
+
+
+		<!-- Hier komt de footer -->
+
+		<?php
+
+
+
+		?>
+	</body>
+</html>
