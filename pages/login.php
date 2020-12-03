49f3e642967e428fc74caa9800f489133d706ae2
MikeVverschuur <72789016+MikeVverschuur@users.noreply.github.com>
2020-12-03 14:17:35 +0100

    ww vergeten

--- pages/login.php
+++ pages/login.php
@@ -3,13 +3,19 @@
         <div class="logo">
             <img src="images/Logo2.png" alt="logo">
         </div>
-            <form method="post">
+            <form method="post" action"">
                 <div class="nl-en">    
                     <a class="nl">Nederlands</a>
                     <a class="en">Engels</a>
                 </div>
                 <input value="<?php if(isset($_COOKIE["member_login"])) { echo $_COOKIE["member_login"]; } ?>" type="text" name="name" placeholder="Gebruikersnaam" required>
                 <input type="password" name="password" placeholder="Wachtwoord" required>
+                <?php
+                    if(isset($_SESSION["error"])){
+                        $error = $_SESSION["error"];
+                        echo "<span>$error</span>";
+                    }
+                ?>  
                 <label class="check">
                     <input type="checkbox" name="remember" id="remember"
                     <?php if(isset($_COOKIE["member_login"])) { ?> checked
@@ -21,25 +27,38 @@
                 </div>
             </form> 
     </div>
-    
+      
 <?php
+    if(isset($_REQUEST['submit'])){
+    $_SESSION['name'] = $_POST['name'];
+    }
+
     if (isset($_POST['name'])) {
         $name = $_POST['name'];
         $pass = $_POST['password'];
+        $error = "username/password incorrect";
         $login = $db->login($name,$pass);
         while ($result = $login->fetch_array(MYSQLI_ASSOC)){
             echo $result['name'];
-            if ($result['role'] == "docent") {
-                exit('hoi');
-            } 
-        }
+            if ($result['role'] == "Docent") {
+                header("Location: docent");
+                die();
+            } else if ($result['role'] == "Student") {
+                header("Location: student");
+                die();
+            }
+        } 
         
         if (! empty($_POST["remember"])) {
         setcookie("member_login", $name);
         } else {
             setcookie("member_login", '');
         }
-    }
+    } else{
+    $_SESSION["error"] = $error;
+    header("location: login.php"); //send user back to the login page.
+}
+
 ?>
 
 </div> 
