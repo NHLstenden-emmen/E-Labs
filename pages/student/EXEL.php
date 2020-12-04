a349e03e138682c2d353843ba41dc0e6446b4d0a
coendokter97 <coendokter97@outlook.com>
2020-12-04 12:11:33 +0100

    csv naar html klaar
    .

--- pages/student/EXEL.php
+++ pages/student/EXEL.php
@@ -1,30 +1,68 @@
-<table width="600">
-<form action="EXEL.php" method="post" enctype="multipart/form-data">
 
-<tr>
-<td width="20%">Select file</td>
-<td width="80%"><input type="file" name="file" id="file" /></td>
-</tr>
-
-<tr>
-<td>Submit</td>
-<td><input type="submit" name="submit" /></td>
-</tr>
-
+<html>
+<head>
+<title>PHP File Upload example</title>
+</head>
+<body>
+ 
+<form action="EXEL.php" enctype="multipart/form-data" method="post">
+Select image :
+<input type="file" name="file"><br/>
+<input type="submit" value="Upload" name="Submit1"> <br/>
+ 
+ 
 </form>
-</table>
+<?php
+$dirname = "uploads/";
+$images = glob($dirname."*.{csv}",GLOB_BRACE);
+$num_of_files = 1;
 
-<?php 
-    if(isset($_POST['submit'])){
-        $name       = $_FILES['file']['name'];  
-        $temp_name  = $_FILES['file']['tmp_name'];  
-        if(isset($name) and !empty($name)){
-            $location = 'uploads/';      
-            if(move_uploaded_file($temp_name, $location.$name)){
-                echo 'File uploaded successfully';
-            }
-        } else {
-            echo 'You should select a file to upload !!';
+
+
+if(isset($_POST['Submit1']))
+{ 
+$filepath ="uploads/" . $_FILES["file"]["name"];
+ 
+if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath)) 
+{
+echo "File is geupload";
+} 
+else 
+{
+echo "Error !!";
+}
+} 
+
+
+$dir = "uploads/";
+$dh = opendir($dir);
+$last = 0;
+$name = "";
+while (($file = readdir($dh)) !== false){
+    if(is_file($dir.$file)){
+        $mt = filemtime($dir.$file);
+        if($mt > $last){
+            $last = $mt;
+            $name = $file;
         }
     }
+}
+closedir($dh);
+
+echo "Last modified file: $name";
+
+echo "<html><body><table width='100%' border='1'>\n\n";
+$f = fopen("uploads/".$name, "r");
+while (($line = fgetcsv($f)) !== false) {
+        echo "<tr>";
+        foreach ($line as $cell) {
+                echo "<td>" . htmlspecialchars($cell) . "</td>";
+        }
+        echo "</tr>\n";
+}
+fclose($f);
+echo "\n</table></body></html>";
 ?>
+
+</body>
+</html>
