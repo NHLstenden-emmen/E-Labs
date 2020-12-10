b3f3a5d1613a1e9410271ebc5a0ac8a3cba61923
esmeekajuiter <esmeekajuiter@gmail.com>
2020-12-10 16:05:27 +0100

    Docent bewerkt student aangepast

--- pages/docent/gebruikersprofiel.php
+++ pages/docent/gebruikersprofiel.php
@@ -2,29 +2,45 @@
 	<h1 id="titelgebruikersprofiel"><?php echo $lang["USER_PROFILE"];?></h1>
 			<img class="profielfototje" src="images/unknown.png" alt="Profielfoto">
 			</br>
+		<form action="gebruikersprofiel.php" method="POST">
 			<button id="Buttonaanpassenprofiel"> <?php echo $lang["ADJUST_PROFILE_PICTURE"];?></button>
 		<div class="Gebruikersprofielcontainer">
 			<div id="Gebruikersprofielstudentinformatierechts"> 
-				<form action=""></form>
+				
 				<p> <?php echo $lang["STUDENT_NUMBER"];?>: </p>
 				<input type="text" name="Studentnummer"/>
-				<p> <?php echo $lang["FIRST_NAME"];?> : </p>
-				<input type="text" name="Voornaam"/>
-				<p> <?php echo $lang["LAST_NAME"];?> : </p>
-				<input type="text" name="Achternaam"/>
-				<p> <?php echo $lang["CLASS"];?> : </p>
-				<input type="text" name="Klas"/>
-			</div>
-			<div id="Gebruikersprofielstudentinformatielinks"> 
-				<p> <?php echo $lang["STUDY"];?>: </p>
-				<input type="text" name="Studie"/>
+				<p> <?php echo $lang["NAME"];?> : </p>
+				<input type="text" name="Naam"/>
 				<p> <?php echo $lang["E-MAIL"];?>: </p>
 				<input type="text" name="E-mail"/>
-				<p> <?php echo $lang["YEAR"];?>: </p>
-				<input type="text" name="Jaar"/>
+			</div>
+			<div id="Gebruikersprofielstudentinformatielinks"> 
+				
 				<p> <?php echo $lang["PASSWORD"];?>: </p>
 				<input type="text" name="Wachtwoord"/>
+				<p> <?php echo $lang["PASSWORD"];?>: </p>
+				<input type="text" name="Wachtwoordnogmaals"/>
 		</div>
 	</div>
-	<button id="Buttonopslaanprofiel">Opslaan</button>
+		<input type="submit" name="submitgegevens" value="Opslaan" id="Buttonopslaanprofiel">
+		<input type="reset" name="resetgegevens" value="Reset" id="Buttonopslaanprofiel">
 </div>
+</form>
+
+<?php
+	var_dump($_POST);
+	if (ISSET($_POST['submitgegevens'])) {
+		$Studentnummer = $_POST['Studentnummer'];
+		$Naam = $_POST['Naam'];
+		$Mail = $_POST['E-mail'];
+		$Wachtwoord = $_POST['Wachtwoord'];
+		$Wachtwoordnog = $_POST['Wachtwoordnogmaals'];
+	}
+	if(!empty($Studentnummer) && !empty($Naam) && !empty($Mail) && !empty($Wachtwoord) && !empty($Wachtwoordnog)){
+		if ($Wachtwoord != $Wachtwoordnog) {
+			die("Wachtwoorden komen niet overeen.");
+		}
+        die("Vul alle velden in s.v.p.");
+    }
+    $db->docentstudentprofielbewerken($Studentnummer, $Naam, $Mail, $Wachtwoord, $Wachtwoordnog);
+?>
