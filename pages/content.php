df3313bbc21b4c30373e7367ee2e4f07f245d162
Kevin Smulders <smulders.kevin.1999@gmail.com>
2020-12-16 20:47:25 +0100

    added users role check to make sure a user cant swich by editing the session

--- pages/content.php
+++ pages/content.php
@@ -2,73 +2,77 @@
 	if (empty($_SESSION['role'])) {
 		include 'login.php'; 
 	} else {
-		if ($_SESSION['role'] == 'Student' && isset($_SESSION['name']) && isset($_SESSION['user_id']) && isset($_SESSION['email'])) {
-			switch(strtolower($pagePath))
-			{
-				case 'e-labs': //file path of your home/start page
-				case 'home':
-				case '':
-					include 'student/home.php';
-					break;
-				case 'labjournaal':
-					include 'student/labjournaal.php';
-					break;
-				case 'editjournaal':
-					include 'student/EditJournaal.php';
-					break;
-				case 'createnewlabjournaal':
-					include 'student/createNewLabjournaal.php';
-					break;
-				case 'updatelabjournaal':
-					include 'student/updatelabjournaal.php';
-					break;
-				case 'exel':
-					include 'student/EXEL.php';
-					break;
-				case 'gebruikersprofiel':
-					include 'student/gebruikersprofiel.php';
-					break;
-				case 'viewlabjournaal':
-					include 'student/viewLabjournaal.php';
-					break;
-				case 'searchresults':
-					include 'searchResults.php';
-					break;
-				default:
-					include '404.php'; // when the page isset found
+		$checkUsersAllowance = $db->checkUsersAllowance($_SESSION['user_id'],$_SESSION['name'],$_SESSION['email']);
+		while ($result = $checkUsersAllowance->fetch_array(MYSQLI_ASSOC)){
+			$_SESSION['role'] = $result['role'];
+			if ($result['role'] == 'Student') {
+				switch(strtolower($pagePath))
+				{
+					case 'e-labs': //file path of your home/start page
+					case 'home':
+					case '':
+						include 'student/home.php';
+						break;
+					case 'labjournaal':
+						include 'student/labjournaal.php';
+						break;
+					case 'editjournaal':
+						include 'student/EditJournaal.php';
+						break;
+					case 'createnewlabjournaal':
+						include 'student/createNewLabjournaal.php';
+						break;
+					case 'updatelabjournaal':
+						include 'student/updatelabjournaal.php';
+						break;
+					case 'exel':
+						include 'student/EXEL.php';
+						break;
+					case 'gebruikersprofiel':
+						include 'student/gebruikersprofiel.php';
+						break;
+					case 'viewlabjournaal':
+						include 'student/viewLabjournaal.php';
+						break;
+					case 'searchresults':
+						include 'searchResults.php';
+						break;
+					default:
+						include '404.php'; // when the page isset found
+				}
+			} else if($result['role'] == 'Docent') {
+				switch(strtolower($pagePath))
+				{
+					case 'e-labs': //file path of your home/start page
+					case 'home':
+					case 'grade':
+					case '':
+						include 'docent/grade.php';
+						break;
+					case 'gebruikersoverzicht':
+						include 'docent/gebruikersoverzicht.php';
+						break;
+					case 'gebruikertoevoegen':
+						include 'docent/gebruikerToevoegen.php';
+						break;
+					case 'gebruikersprofiel':
+						include 'docent/gebruikersprofiel.php';
+						break;
+					case 'labjournaalview':
+						include 'docent/labjournaalView.php';
+						break;
+					case'acountbewerken':
+						include 'docent/editAccount.php';
+						break;
+					case 'searchresults':
+						include 'searchResults.php';
+						break;
+					default:
+						include '404.php'; // when the page isset found
+				}
+			} else{
+				include '404.php';
 			}
-		} else if($_SESSION['role'] == 'Docent' && isset($_SESSION['name']) && isset($_SESSION['user_id']) && isset($_SESSION['email'])) {
-			switch(strtolower($pagePath))
-			{
-				case 'e-labs': //file path of your home/start page
-				case 'home':
-				case 'grade':
-				case '':
-					include 'docent/grade.php';
-					break;
-				case 'gebruikersoverzicht':
-					include 'docent/gebruikersoverzicht.php';
-					break;
-				case 'gebruikertoevoegen':
-					include 'docent/gebruikerToevoegen.php';
-					break;
-				case 'gebruikersprofiel':
-					include 'docent/gebruikersprofiel.php';
-					break;
-				case 'labjournaalview':
-					include 'docent/labjournaalView.php';
-					break;
-				case'acountbewerken':
-					include 'docent/editAccount.php';
-					break;
-				case 'searchresults':
-					include 'searchResults.php';
-					break;
-				default:
-					include '404.php'; // when the page isset found
-			}
-		} else{
-			include '404.php';
 		}
 	}
 ?>
