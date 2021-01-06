715faf0d0d03d1e273154edb2755380c8ef63f45
coendokter97 <coendokter97@outlook.com>
2021-01-06 14:17:01 +0100

    responsive edit journaal
    .

--- pages/student/EditJournaal.php
+++ pages/student/EditJournaal.php
@@ -61,107 +61,119 @@ if (empty($message) && isset($_GET['id'])) {
 		if ($result["submitted"] == 0) {
 		?>
 	<form method="post" enctype='multipart/form-data' class="newlabjournaalcontainer">
-		<div>
-			<label for="title"><?php echo $lang["TITLE"];?>:</label> </br>
-			<input type="text" name="title" class="nieuwetitellabjournaal" value="<?php echo $result['title'];?>">
-			<!-- </form> -->
-		</div>
-		<div>
-				<?php
-					echo "<label for='users'><a class='help'><i class='fas fa-question-circle' Title='Ctrl + Linkermuisknop om meerdere te selecteren'></i></a>".$lang['OTHERSTUDENTS']."</label>";
-					$student = $db->selectStudents();
-					echo '<div class="twoinone"><div><select name="medestudenten" multiple>';
-					while ($user = $student->fetch_array(MYSQLI_ASSOC)){
-						if($user['user_id'] !== $_SESSION['user_id'] && $user['user_id'] !== $result['creater_id']){
-							echo "<option value='".$user['user_id']."'";
+		<div class="form-row">
+			<div class="col-md-4 mb-3 offset-1">
+				<label for="title"><?php echo $lang["TITLE"];?>:</label> </br>
+				<input type="text" name="title" class="nieuwetitellabjournaal" value="<?php echo $result['title'];?>">
+				<!-- </form> -->
+			</div>
+			<div class="col-md-4 mb-3 offset-1">
+					<?php
+						echo "<label for='users'><a class='help'><i class='fas fa-question-circle' Title='Ctrl + Linkermuisknop om meerdere te selecteren'></i></a>".$lang['OTHERSTUDENTS']."</label>";
+						$student = $db->selectStudents();
+						echo '<div class="twoinone"><div><select name="medestudenten" multiple>';
+						while ($user = $student->fetch_array(MYSQLI_ASSOC)){
+							if($user['user_id'] !== $_SESSION['user_id'] && $user['user_id'] !== $result['creater_id']){
+								echo "<option value='".$user['user_id']."'";
+								$labusers = $db->GetAllLabUsers($_GET['id']);
+								while($labuser = $labusers->fetch_array(MYSQLI_ASSOC)){
+									if($user['user_id'] === $labuser['user_id']){
+										echo "selected='selected'";
+									}
+								}
+								echo ">".$user['name']."</option>";
+							}
+						}
+						echo "</select></div><div>";
+						if($_SESSION['user_id'] == $result['creater_id']){
 							$labusers = $db->GetAllLabUsers($_GET['id']);
-							while($labuser = $labusers->fetch_array(MYSQLI_ASSOC)){
-								if($user['user_id'] === $labuser['user_id']){
-									echo "selected='selected'";
+							echo "<ul><pre>";
+							while ($users = $labusers->fetch_array(MYSQLI_ASSOC)){
+								if($users['user_id'] !== $result['creater_id']){
+									$username = $users['name'];
+									echo "<li>".$username."&#9;&#9;<button name='delete' value='".$users['user_id']."'>Delete</button></li>";
 								}
 							}
-							echo ">".$user['name']."</option>";
+							echo "</pre></ul>";
 						}
-					}
-					echo "</select></div><div>";
-					if($_SESSION['user_id'] == $result['creater_id']){
-						$labusers = $db->GetAllLabUsers($_GET['id']);
-						echo "<ul><pre>";
-						while ($users = $labusers->fetch_array(MYSQLI_ASSOC)){
-							if($users['user_id'] !== $result['creater_id']){
-								$username = $users['name'];
-								echo "<li>".$username."&#9;&#9;<button name='delete' value='".$users['user_id']."'>Delete</button></li>";
-							}
+						echo "</div></div>";
+						if(isset($_POST['delete'])){
+							$deleteuser = $_POST['delete'];
+							$delete = $db->DeleteExtraUser($deleteuser, $_GET['id']);
+							echo $delete;
 						}
-						echo "</pre></ul>";
-					}
-					echo "</div></div>";
-					if(isset($_POST['delete'])){
-						$deleteuser = $_POST['delete'];
-						$delete = $db->DeleteExtraUser($deleteuser, $_GET['id']);
-						echo $delete;
-					}
-					?>
+						?>
+			</div>
 		</div>
 		<!-- <form method="post" class="newlabjournaalcontainer"> -->
-		<div>
-			<label for="Goal"><?php echo $lang["GOAL"];?>:</label> </br>
-				<textarea class="groteretextarealabjournaal" name="Goal" value="<?php echo $result['Goal'];?>"><?php echo $result['Goal'];?></textarea>
+		<div class="form-row">
+			<div class="col-md-4 mb-3 offset-1">
+				<label for="Goal"><?php echo $lang["GOAL"];?>:</label> </br>
+					<textarea class="groteretextarealabjournaal" name="Goal" value="<?php echo $result['Goal'];?>"><?php echo $result['Goal'];?></textarea>
+			</div>
+			<div class="col-md-4 mb-3 offset-1">
+				<label for="Hypothesis"><?php echo $lang["HYPOTHESIS"];?>:</label> </br>
+					<textarea class="groteretextarealabjournaal" name="Hypothesis" value="<?php echo $result['Hypothesis'];?>"><?php echo $result['Hypothesis'];?></textarea>
+			</div>
 		</div>
-		<div>
-			<label for="Hypothesis"><?php echo $lang["HYPOTHESIS"];?>:</label> </br>
-				<textarea class="groteretextarealabjournaal" name="Hypothesis" value="<?php echo $result['Hypothesis'];?>"><?php echo $result['Hypothesis'];?></textarea>
+		<div class="form-row">
+			<div class="col-md-4 mb-3 offset-1">
+				<label for="theory"><?php echo $lang["THEORY"];?>:</label> </br>
+					<textarea class="groteretextarealabjournaal" name="theory" value="<?php echo $result['theory'];?>"><?php echo $result['theory'];?></textarea>
+			</div>
+			<div class="col-md-4 mb-3 offset-1">
+				<label for="safety"><?php echo $lang["SAFETY"];?>:</label> </br>
+					<textarea class="groteretextarealabjournaal" name="safety" value="<?php echo $result['safety'];?>"><?php echo $result['safety'];?></textarea>
+			</div>
 		</div>
-		<div>
-			<label for="theory"><?php echo $lang["THEORY"];?>:</label> </br>
-				<textarea class="groteretextarealabjournaal" name="theory" value="<?php echo $result['theory'];?>"><?php echo $result['theory'];?></textarea>
+		<div class="form-row">
+			<div class="col-md-4 mb-3 offset-1">
+				<label for="logboek"><?php echo $lang["LOGBOOK"];?>:</label> </br>
+					<textarea class="groteretextarealabjournaal" name="logboek" value="<?php echo $result['logboek'];?>"><?php echo $result['logboek'];?></textarea>
+			</div>
+			<div class="col-md-4 mb-3 offset-1">
+				<label for="method_materials"><?php echo $lang["METHOD_MATERIALS"];?>:</label> </br>
+					<textarea class="groteretextarealabjournaal" name="method_materials" value="<?php echo $result['method_materials'];?>"><?php echo $result['method_materials'];?></textarea>
+			</div>
 		</div>
-		<div>
-			<label for="safety"><?php echo $lang["SAFETY"];?>:</label> </br>
-				<textarea class="groteretextarealabjournaal" name="safety" value="<?php echo $result['safety'];?>"><?php echo $result['safety'];?></textarea>
-		</div>
-		<div>
-			<label for="logboek"><?php echo $lang["LOGBOOK"];?>:</label> </br>
-				<textarea class="groteretextarealabjournaal" name="logboek" value="<?php echo $result['logboek'];?>"><?php echo $result['logboek'];?></textarea>
-		</div>
-		<div>
-			<label for="method_materials"><?php echo $lang["METHOD_MATERIALS"];?>:</label> </br>
-				<textarea class="groteretextarealabjournaal" name="method_materials" value="<?php echo $result['method_materials'];?>"><?php echo $result['method_materials'];?></textarea>
-		</div>
-		<div>
-		
-			<label for="year"><?php echo $lang["YEAR"];?>:</label> </br>
-			<label for="1"><?php echo $lang["YEAR_1"];?></label>
-				<input type="radio" name="year" value="1" <?php if($year == 1){echo 'checked';}?>>
-			<label for="1"><?php echo $lang["YEAR_2"];?></label>
-				<input type="radio" name="year" value="2"<?php if($year == 2){echo 'checked';}?>>
-			<label for="1"><?php echo $lang["YEAR_3"];?></label>
-				<input type="radio" name="year" value="3"<?php if($year == 3){echo 'checked';}?>>
-		</div>
-		<div>
-			<label for="fileupload"><?php echo $lang["UPLOAD_FILE"];?>:</label></br>
-			<input name='fileupload' type='file'>
-			<?php 
-				// check if its a img of excel file
-				// if (condition) {
-					echo '<img src="'.$result["Attachment"].'" alt="" srcset="">';
-				// } else if (condition){
-				// 	# code...
-				// } else if (empty($result["Attachment"])){
-				// 	# code...
-				// } else {
-				// 	# code...
-				// }
-				
-			?>
+		<div class="form-row">
+			<div class="col-md-4 mb-3 offset-1">
 			
-		</div>	
-		<div>
-			<input type="submit" name="Opslaan" value="<?php echo $lang["SAVE"];?>">
-			<input type="submit" name="Inleveren" value="<?php echo $lang["HAND_IN"];?>">
+				<label for="year"><?php echo $lang["YEAR"];?>:</label> </br>
+				<label for="1"><?php echo $lang["YEAR_1"];?></label>
+					<input type="radio" name="year" value="1" <?php if($year == 1){echo 'checked';}?>>
+				<label for="1"><?php echo $lang["YEAR_2"];?></label>
+					<input type="radio" name="year" value="2"<?php if($year == 2){echo 'checked';}?>>
+				<label for="1"><?php echo $lang["YEAR_3"];?></label>
+					<input type="radio" name="year" value="3"<?php if($year == 3){echo 'checked';}?>>
+			</div>
+			<div class="col-md-4 mb-3 offset-1">
+				<label for="fileupload"><?php echo $lang["UPLOAD_FILE"];?>:</label></br>
+				<input name='fileupload' type='file'>
+				<?php 
+					// check if its a img of excel file
+					// if (condition) {
+						echo '<img src="'.$result["Attachment"].'" alt="" srcset="">';
+					// } else if (condition){
+					// 	# code...
+					// } else if (empty($result["Attachment"])){
+					// 	# code...
+					// } else {
+					// 	# code...
+					// }
+					
+				?>
+				
+			</div>	
 		</div>
-		<div>
-			<input type="reset" name="reset" value="<?php echo $lang["RESET"];?>"> </br>
+		<div class="form-row">
+			<div class="col-md-4 mb-3 offset-1">
+				<input type="submit" name="Opslaan" value="<?php echo $lang["SAVE"];?>">
+				<input type="submit" name="Inleveren" value="<?php echo $lang["HAND_IN"];?>">
+			</div>
+			<div class="col-md-4 mb-3 offset-1">
+				<input type="reset" name="reset" value="<?php echo $lang["RESET"];?>"> </br>
+			</div>
 		</div>
 	</form>
 	<?php  }}
