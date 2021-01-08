2d72f0dea8287662961fa5a76fa32f0a5acdf90f
MikeVverschuur <72789016+MikeVverschuur@users.noreply.github.com>
2021-01-08 10:36:01 +0100

    about us pagina

--- inc/header.php
+++ inc/header.php
@@ -65,6 +65,9 @@
 					case 'createnewlabjournaal'://file path of nieuwlabjournaal
 						echo '<link rel="stylesheet" href="css/pages/nieuwlabjournaal.css">';
 						break;
+					case 'aboutus':
+						echo '<link rel="stylesheet" href="css/pages/aboutus.css">';
+						break;
 					default:
 						echo '<link rel="stylesheet" href="css/pages/404.css">';
 						break;
@@ -93,9 +96,13 @@
 					case 'labjournaalview':
 						echo '<link rel="stylesheet" href="css/pages/labjournaalview.css">';
 						break;
+					case 'aboutus':
+						echo '<link rel="stylesheet" href="css/pages/aboutus.css">';
+						break;
 					default:
 						echo '<link rel="stylesheet" href="css/pages/404.css">';
 						break;
+
 				}
 			} else{
 				echo '<link rel="stylesheet" href="css/pages/404.css">';
