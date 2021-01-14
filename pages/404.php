007b6a028645262abe7a9bf5c9f285ec9e03f2ad
Kevin Smulders <smulders.kevin.1999@gmail.com>
2021-01-14 12:45:21 +0100

    added max width to image

--- pages/404.php
+++ pages/404.php
@@ -1,4 +1,4 @@
 <p class="d-flex justify-content-center">404</p>
 <div class="d-flex justify-content-center">
-<img src="images/404.png" alt="de 404 pagina ">
+	<img src="images/404.png" style="max-width: 90%;" alt="de 404 pagina ">
 </div>
